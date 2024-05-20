7ff5e46235c8c2f1dbb322a5d618e7c1efe97ff6
tars919 <138830040+tars919@users.noreply.github.com>
2024-05-20 13:30:35 -0400

    Create MovieRecommendationAppApp.swift

---
+++ MovieRecommendationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRecommendationAppApp.swift
+//  MovieRecommendationApp
+//
+//  Created by Tarika Selvaraj on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieRecommendationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
